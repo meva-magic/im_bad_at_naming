c6fdf6227beb6bf504b91c9574eac2d03bb3e797
saharov-dev <saharov.dev@gmail.com>
2025-01-19 01:33:34 +0300

    movement

---
+++ uroboros/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed;
+    public Vector2 forceToApply;
+    public Vector2 PlayerInput;
+    public float forceDamping;
+    void Update()
+    {
+        PlayerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+    void FixedUpdate()
+    {
+        Vector2 moveForce = PlayerInput * moveSpeed;
+        moveForce += forceToApply;
+        forceToApply /= forceDamping;
+        if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
+        {
+            forceToApply = Vector2.zero;
+        }
+        rb.linearVelocity = moveForce;
+    }
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            forceToApply += new Vector2(-20, 0);
+            Destroy(collision.gameObject);
+        }
+    }
+}
